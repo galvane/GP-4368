c94ae267147e18b203c8c94d3dfea479019ffde1
galvane <lgalvanevelyn@gmail.com>
2019-04-01 12:55:03 -0500

    created class GUI

--- venv/main.py
+++ venv/main.py
@@ -1,20 +1,24 @@
 from tkinter import *
 
-main_window = Tk()
+class GUI:
+    def __init__(self):
+        self.main_window = Tk()
+        self.main_window.title("Reinforcement Learning")
+        self.main_window.geometry("500x200")
 
-main_window.title("Reinforcement Learning")
-main_window.geometry("500x200")
+    def new_window(self, title):
+        window = Toplevel(self.main_window)
+        window.title("PD-World")
+        window.geometry("500x500")
 
-def new_window():
-    window = Toplevel(main_window)
-    window.title("PD World")
-    window.geometry("500x500")
+    def createPDWorld(self):
+        self.view_world_button = Button(self.main_window, text ='View World', pady = 10 ,width = 25, background = '#4d88ff', command = lambda:self.new_window("PD-World"))
 
-view_world_button = Button(main_window, text ='View World', pady = 10 ,width = 25, background = '#4d88ff', command = new_window)
-close_button = Button(main_window, text = 'Close', pady = 20, width = 25, command = main_window.destroy)
+    def generate(self):
+        # add to main window
+        self.view_world_button.pack()
+        self.main_window.mainloop()
 
-# add to main window
-view_world_button.pack()
-close_button.pack()
-
-main_window.mainloop()
+gui = GUI()
+gui.createPDWorld()
+gui.generate()
