88c557d0d5296bc34975e406b9b5565fdd62f592
AllanKey <allankey4@gmail.com>
2019-04-04 15:07:39 -0500

    Adding button to UI to show Q-Table

--- venv/main.py
+++ venv/main.py
@@ -22,15 +22,22 @@ class GUI:
     def create_pdworld(self):
         self.view_world_button = Button(self.main_window, text='View World', pady=10, width=25, background='#4d88ff',
                                         command=lambda: self.new_window("PD-World"))
-        square1 = Frame(self.main_window, bg="red")
-        square1.grid(row=0, column=0, rowspan=3, columnspan=2, sticky=W+E+N+S)
 
         self.view_world_button.grid()
         self.view_world_button.place(relx=0.5, rely=0.1, anchor=CENTER)
+
+
+    def create_qTable(self):
+        self.view_qTable_button = Button(self.main_window, text='View Q-Table', pady=10, width=25, background='#4d88ff',
+                                         command=lambda: self.new_window("Q-Table"))
+
+        self.view_qTable_button.grid()
+        self.view_qTable_button.place(relx=0.5, rely=.25, anchor=S)
 
     def generate(self):
         self.main_window.mainloop()
 
 gui = GUI()
 gui.create_pdworld()
+gui.create_qTable()
 gui.generate()
