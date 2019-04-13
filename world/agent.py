f00c9dd7e7fa571ee8ec735d6196ef35dd003413
galvane <lgalvanevelyn@gmail.com>
2019-04-13 14:41:09 -0500

    worked on validateMove(), move(), and getApplicability() methods

--- world/agent.py
+++ world/agent.py
@@ -2,29 +2,32 @@ from tkinter import *
 from enum import Enum
 import collections
 
+from world.cell import Cell, CellType
 
-class OperatorType(Enum):
+
+class ActionType(Enum):
     NORTH = 1
     EAST = 2
     SOUTH = 3
     WEST = 4
+    PICKUP = 5
+    DROPOFF = 6
 
-class Operator:
-    direction = None
-    isApplicable = True
+class Action:
+    actionType = None
     qValue = 0
     def __init__(self,  type):
-        self.direction = type
+        self.actionType = type
 
     def setApplicability(self, isApplicable):
         self.isApplicable = isApplicable
 
-    def getApplicability(self, operator):
-        return operator.isApplicable
+    def getApplicability(self, agent):
+        return agent.move(self.actionType)
 
 class Agent:
     world = None
-    position = None
+    agentPosition = None
     img = None
     operators = []
 
@@ -32,58 +35,69 @@ class Agent:
         self.img = PhotoImage(file="frog.png")
         self.img = self.img.subsample(15)
         self.world = world
-        self.position = world.startCell
+        self.agentPosition = world.startCell
 
-        op = [Operator(OperatorType.NORTH),
-              Operator(OperatorType.EAST),
-              Operator(OperatorType.SOUTH),
-              Operator(OperatorType.WEST)]
+        op = [Action(ActionType.NORTH),
+              Action(ActionType.EAST),
+              Action(ActionType.SOUTH),
+              Action(ActionType.WEST)]
         self.operators.append(op)
 
     """PD-WORLD FOR SOME REASON HAS X AND Y COORDINATES FLIPPED"""
-    def move(self, direction):
+    def move(self, action_type):
 
-        x = self.position[0]
-        y = self.position[1]
+        canMove = True
+        x = self.agentPosition[0]
+        y = self.agentPosition[1]
 
-        oldPosition = self.position
-        if direction == OperatorType.NORTH:
+        oldPosition = self.agentPosition
+        if action_type == ActionType.NORTH:
             self.newx = x - 1
-            self.position = self.validatePosition(self.newx, y)
-        elif direction == OperatorType.EAST:
+            self.agentPosition = self.validateMove(self.newx, y)
+        elif action_type == ActionType.EAST:
             self.newy = y + 1
-            self.position = self.validatePosition(x, self.newy)
-        elif direction == OperatorType.SOUTH:
+            self.agentPosition = self.validateMove(x, self.newy)
+        elif action_type == ActionType.SOUTH:
             self.newx = x + 1
-            self.position = self.validatePosition(self.newx, y)
-        elif direction == OperatorType.WEST:
+            self.agentPosition = self.validateMove(self.newx, y)
+        elif action_type == ActionType.WEST:
             self.newy = y - 1
-            self.position = self.validatePosition(x, self.newy)
+            self.agentPosition = self.validateMove(x, self.newy)
+        elif action_type == ActionType.PICKUP:
+            canMove = self.agentPosition.blocks > 0
+        elif action_type == ActionType.DROPOFF:
+            canMove = self.agentPosition.blocks < 5
         else:
             print("[WARNING]: " + "Invalid operator!")
+            canMove = False
 
 
         print("applied action: " ,end ="")
-        print(direction, end = "")
-        print(" ==> agent moved from " + "("+','.join(map(str, oldPosition))+")" + " to " + "("+','.join(map(str, self.position))+")")
+        print(action_type, end ="")
+        print(" ==> agent moved from " + "(" +','.join(map(str, oldPosition)) +")" + " to " + "(" +','.join(map(str, self.agentPosition)) + ")")
+
+        return canMove
 
 
-    def validatePosition(self, x, y):
-        point = collections.namedtuple('point', ['x', 'y'])
+    def validateMove(self, x, y):
         newx = x
         newy = y
         if self.validateNewX(x) and self.validateNewY(y):
-            self.position = point(newx, newy)
-            return self.position
+            newCell = self.world.getCell(newx, newy)
+            self.agentPosition = newCell
+            return self.agentPosition
         elif self.validateNewX(x) and not self.validateNewY(y):
-            self.position = point(newx, y)
-            return self.position
+            newCell = self.world.getCell(newx, y)
+            self.agentPosition = newCell
+            return self.agentPosition
         elif not self.validateNewX(x) and self.validateNewY(y):
-            self.position = point(x, newy)
-            return self.position
+            newCell = self.world.getCell(x, newy)
+            self.agentPosition = newCell
+            return self.agentPosition
         else:
-            self.position = point(x, newy)
-            return self.position
+            newCell = self.world.getCell(x, y)
+            self.agentPosition = newCell
+            return self.agentPosition
 
     def validateNewX(self, x):
         if x > 5 or x < 0:
